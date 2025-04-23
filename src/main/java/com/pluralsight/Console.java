23ef105ef04b7f1e327c80760eb552283953cc6a
arthurmov <officialamovsesyan@gmail.com>
2025-04-23 14:44:03 -0400

    Saving Changes

--- src/main/java/com/pluralsight/Console.java
+++ src/main/java/com/pluralsight/Console.java
@@ -6,9 +6,20 @@ public class Console {
     Scanner scanner = new Scanner(System.in);
 
     public int promptForInt(String prompt) {
-        System.out.println(prompt);
-        int result = scanner.nextInt();
-        scanner.nextLine();
+        boolean hasResult = false;
+        int result = -1;
+        while (!hasResult) {
+            try {
+                System.out.println(prompt);
+                int result = scanner.nextInt();
+                scanner.nextLine();
+                return result;
+
+            } catch (Exception e) {
+                System.out.println("Invalid Entry, try again!");
+            }
+        }
+        
         return result;
     }
 
